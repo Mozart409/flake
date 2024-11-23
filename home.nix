3b6c70ce0b122eae1e5f5c8c54adec7a91385873
Amadeus Mader <amadeus@mozart409.com>
2024-11-23 19:30:04 +0100

    feat: :sparkels: added tailscale

--- home.nix
+++ home.nix
@@ -175,7 +175,9 @@
     };
   };
 
-
+  programs.tailscale = {
+	enable = true;
+  };
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
